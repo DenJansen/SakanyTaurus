d517d9f01a6df2196599150abfe34c23c73530f0
DenJansen <curt.justice1@gmail.com>
2020-03-19 18:47:37 -0700

    Added location lists and other changes to main view

--- sakanytaurus/sakanyapp/urls.py
+++ sakanytaurus/sakanyapp/urls.py
@@ -5,5 +5,7 @@ app_name = 'sakanyapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_page, name='login'),
-    path('user_login/', views.user_login, name='user_login',)
+    path('user_login/', views.user_login, name='user_login'),
+    path('findpub/', views.findpub, name='findpub'),
+    path('newcrawl/', views.newcrawl, name='newcrawl')
 ]
--- sakanytaurus/sakanyapp/views.py
+++ sakanytaurus/sakanyapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from . import secrets
 
+from . import secrets
+from .models import UserRoute, RouteStep
+
+import requests
 import json
 
 # Create your views here.
@@ -49,3 +52,58 @@ def index(request):
         'mapapi': secrets.mapapi
     }
     return render(request, 'sakanyapp/index.html', context)
+
+@login_required
+def findpub(request):
+    save_json = json.loads(request.body)
+    address = save_json['address']
+    #access api using address string and api key
+    map_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={secrets.mapapi}'
+    map_response = requests.get(map_url)
+    #pull coordinates
+    geocoords = json.loads(map_response.text)['results'][0]['geometry']['location']
+
+    yelpurl = f"https://api.yelp.com/v3/businesses/search?latitude={geocoords['lat']}&longitude={geocoords['lng']}&sort_by=distance&categories=breweries,brewpubs,beerbar,divebars,irish_pubs,pubs,beergarden,beergardens,barcrawl,beerhall"
+
+    yelpheaders = {'Authorization': 'Bearer %s' % secrets.yelpapi}
+    yelpresponse = requests.get(yelpurl, headers=yelpheaders)
+    yelpdata = json.loads(yelpresponse.text)['businesses']
+    #
+    origin = (geocoords['lat'],geocoords['lng'])
+
+    # pubresults = {}
+    # for x in yelpdata:
+    #     pubresults.append((x['name'],x['location']['display_address'][0],x['location']['display_address'][-1],x['distance']))
+    #
+    # print(pubresults)
+    # counter = 0
+    # for i in yelpdata:
+    #     key = str(counter)
+    #     pubresults[key] = i
+    #     counter += 1
+    #
+    # print(pubresults)
+    #
+    # return HttpResponse("Success!")
+
+    return JsonResponse({'pubresults':yelpdata,'origin':origin})
+
+@login_required
+def newcrawl(request):
+    pub_route = json.loads(request.body)
+    print(pub_route)
+    # route = pub_route['route']
+    # order_num = pub_route['order_num']
+    # loc_name = pub_route['loc_name']
+    # loc_rating = pub_route['loc_rating']
+    # loc_address = pub_route['loc_address']
+    # loc_lat = pub_route['loc_lat']
+    # loc_lon = pub_route['loc_lon']
+    # open_time = pub_route['open_time']
+    # close_time = pub_route['close_time']
+    # arr_time = pub_route['arr_time']
+    # visited = pub_route['visited']
+    #
+    # create_crawl = RouteStep
+
+    return HttpResponse("Success!")
